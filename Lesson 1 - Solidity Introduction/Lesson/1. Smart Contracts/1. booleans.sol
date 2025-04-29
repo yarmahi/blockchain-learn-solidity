53b882317bb45316a5143edfdfd485e264d08657
yarmahi <mahilike0@gmail.com>
2025-04-29 08:42:23 +0300

    Lesson 1 Smart Contracts Done

---
+++ Lesson 1 - Solidity Introduction/Lesson/1. Smart Contracts/1. booleans.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Contract {
+    // Declare public boolean storage variables with initial values
+    bool public a = true;
+    bool public b = false;
+}
