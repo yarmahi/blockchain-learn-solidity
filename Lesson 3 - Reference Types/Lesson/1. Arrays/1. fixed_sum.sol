032bda64cfad5b7ec3099151d8d1e2bb837baed9
yarmahi <mahilike0@gmail.com>
2025-05-02 20:34:29 +0300

    Lesson 3 - Arrays Done

---
+++ Lesson 3 - Reference Types/Lesson/1. Arrays/1. fixed_sum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    function sum(uint256[5] calldata arr) external pure returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+    }
+}
